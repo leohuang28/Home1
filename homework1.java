9097edf214b527a2e3e38c810cd1febdd5331197
leohuang28 <leohuang28@yahoo.com.tw>
2014-02-23 19:55:11 +0800

    week1 hw

---
+++ homework1.java
@@ -0,0 +1,9 @@
+class week1 {
+  public static void main(String args[])  { 
+	System.out.println("《金陵圖》作者：韋庄\n\n");
+	System.out.println("江雨霏霏江草齊，\n");
+	System.out.println("六朝如夢鳥空啼。\n");
+	System.out.println("無情最是台城柳，\n");
+	System.out.println("依舊煙籠十里堤。\n");
+  }
+}
